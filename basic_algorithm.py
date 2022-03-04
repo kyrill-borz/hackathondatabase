4915d624045993043f3d3b37516b1dd05080ba92
ensc <>
2022-03-04 16:52:37 +0000

    Remove old import

--- basic_algorithm.py
+++ basic_algorithm.py
@@ -8,7 +8,6 @@ import json
 # External imports
 import numpy as np
 import pandas as pd
-from calculate_flight_distance import calculate_flight_distance
 
 # Constants
 EARTH_RADIUS = 6.371e6 # Average radius of the Earth in metres
