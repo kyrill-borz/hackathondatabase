94d1da0637c8c6172223dd4254d4a99779f5cefd
John <accounts@johnjessop.uk>
2022-03-04 14:43:06 +0000

    Fixed Eve's script

--- basic_algorithm.py
+++ basic_algorithm.py
@@ -2,6 +2,7 @@
 
 # External imports
 import pandas as pd
+import numpy as np
 
 def load_data(csv_file):
     """ Loads the CSV file and filters to components of interest. """
@@ -13,20 +14,18 @@ def load_data(csv_file):
 
 def create_score(df, money_weight=100, eco_weight=100, speed_weight=100):
     """ Basic scoring system. """
-    print(df)
 
     df2 = pd.DataFrame(df["Material: Name"])
     df2.insert(1, "Company name", df["Company name"])
 
-    df2["money_score"] = round(100 - (df["Average cost/cubic metre"]/2), 0).astype(int)
+    df2["Money score (%)"] =  (100 - (df["Average cost/cubic metre"]/2)).round().astype(int)
 
-    df2["eco_score"] = round(100 - (df["Overall EC sum (tonCO2e)"] * 10000),0).astype(int)
+    df2["Eco score (%)"] = (100 - (df["Overall EC sum (tonCO2e)"] * 10000)).round().astype(int)
 
-    df2["speed_score"] = df["Construction times"] * 10
+    df2["Speed score (%)"] = df["Construction times"] * 10
+
+    df2["Overall score (%)"] = (((df2["Money score (%)"]*money_weight) + (df2["Eco score (%)"]*eco_weight) + (df2["Speed score (%)"]*speed_weight)) /  (money_weight + eco_weight + speed_weight)).astype(int)
     print(df2)
-
-    df2["overall_score"] = ((df["money_score"]*money_weight) + (df["eco_score"]*eco_weight) + (df["speed_score"]*speed_weight)) /  (money_weight + eco_weight + speed_weight)
-    # print(df2)
     return df2
 
 df = load_data("static/data/concrete.csv")
